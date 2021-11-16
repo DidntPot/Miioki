f09ffc47827f88529cddf5f642d61cc8797aae8e
Peppa <79522460+DidntPot@users.noreply.github.com>
2021-11-15 20:39:20 -0800

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "Miioki";
+const NAME = "PocketMine-MP";
 const BASE_VERSION = "3.13.0";
 const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
